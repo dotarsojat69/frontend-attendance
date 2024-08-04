2e337bf0d49089f44f37228764b17cf109a11313
dotarsojat69 <tdeath457@gmail.com>
2024-08-04 16:58:46 +0700

    feat: add api types

---
+++ src/utils/types/api.ts
@@ -0,0 +1,37 @@
+export interface IResponse {
+    code: number;
+    message: string;
+  }
+  
+  export interface IResponseData<TDatas> {
+    code: number;
+    data: TDatas;
+    message: string;
+  }
+  
+  export interface LoginPayload {
+    token: string;
+  }
+  
+  export interface IResponsePagination<TDatas> {
+    code: number;
+    message: string;
+    data: TDatas;
+    pagination: {
+      page: number;
+      page_size: number;
+      total_pages: number;
+    };
+  }
+  
+  export interface ITokenData {
+    exp: number;
+    iat: number;
+    id: string;
+    is_active: number;
+    is_admin: boolean;
+  }
+  
+  export interface Request {
+    status?: string;
+  }
