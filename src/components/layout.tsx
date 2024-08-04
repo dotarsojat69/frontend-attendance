ef5f5fd7c6062f145aeb5c8b117cace5f3cfd6d5
dotarsojat69 <tdeath457@gmail.com>
2024-08-04 14:55:28 +0700

    feat: add bottom bar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Bottombar from "./bottombar";
 
 interface Props {
   children: ReactNode;
@@ -10,12 +11,9 @@ export default function Layout(props: Props) {
   return (
     <div className="flex justify-center bg-slate-900 xs:hidden">
       <div className="layout-container min-w-full max-w-full bg-white dark:bg-neutral-800 md:min-w-[480px] md:max-w-[480px]">
-
-      
         {children}
-      </div>
-      
-      
+      <Bottombar />
+      </div>     
     </div>
   );
 }
