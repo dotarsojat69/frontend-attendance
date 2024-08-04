5ffc6cf9707eda74cbb17cfaaf4867f1e8e58557
dotarsojat69 <tdeath457@gmail.com>
2024-08-04 16:58:32 +0700

    feat: call api for auth

---
+++ src/utils/apis/auth/api.ts
@@ -0,0 +1,23 @@
+import axiosWithConfig from "@/utils/axiosWithConfig";
+import { IResponse, IResponseData, LoginPayload } from "@/utils/types/api";
+import { LoginType, RegisterType } from "./types";
+
+export const userLogin = async (body: LoginType) => {
+  try {
+    const response = await axiosWithConfig.post(`/login`, body);
+
+    return response.data as IResponseData<LoginPayload>;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
+
+export const userRegister = async (body: RegisterType) => {
+  try {
+    const response = await axiosWithConfig.post(`/users`, body);
+
+    return response.data as IResponse;
+  } catch (error: any) {
+    throw Error(error.response.data.message);
+  }
+};
