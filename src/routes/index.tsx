0767eafd669ff062a2f1dd10caf8ad9cb6e1a348
dotarsojat69 <tdeath457@gmail.com>
2024-08-04 14:56:40 +0700

    feat: add auth routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,16 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Homepage from "@/page/App";
+import Login from "@/page/auth/login";
+import Register from "@/page/auth/register";
 
 const router = createBrowserRouter([
   {
     
     children: [
       { path: "/", element: <Homepage /> },
-      
+      { path: "/login", element: <Login /> },
+      { path: "/register", element: <Register /> },
     ],
   },
 ]);
